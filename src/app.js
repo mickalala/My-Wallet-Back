1c59d0f2540531b535e215a235f82f7fe6b15e95
mickalala <mickalarissa01@gmail.com>
2023-04-22 20:58:07 -0300

    Parte do login semi-finalizada

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
+import { v4 as uuid } from "uuid"
 
 const app = express()
 
@@ -26,7 +27,7 @@ const siginSchema = joi.object({
 app.post("/cadastro", async (req, res) => {
     const { name, email, password } = req.body
     const validation = siginSchema.validate(req.body, { abortEarly: false })
-    const passwordHash = bcrypt.hashSync(password,1)
+    const passwordHash = bcrypt.hashSync(password, 1)
 
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message)
@@ -36,12 +37,41 @@ app.post("/cadastro", async (req, res) => {
     const existenUser = await db.collection("users").findOne({ email: email })
     if (existenUser) res.sendStatus(409)
     try {
-        await db.collection("users").insertOne({ name, email, passwordHash })
+        await db.collection("users").insertOne({ name, email, password:passwordHash })
 
         return res.sendStatus(201)
     } catch (error) {
         res.status(500).send(err.message)
     }
+})
+
+app.post("/", async (req, res) => {
+    const { email, password } = req.body
+    const userSchema= joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    })
+    const validation= userSchema.validate(req.body, {abortEarly:false})
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message)
+        console.log(errors)
+        return res.status(422).send(errors)
+    }
+    try {
+        const user = await db.collection("users").findOne({ email })
+        if (!user) return res.sendStatus(404)
+
+        const rightPassword = bcrypt.compareSync(password, user.password)
+        if (!rightPassword) return res.sendStatus(401)
+
+        const token = uuid()
+        await db.collection("sessoes").insertOne({ token, userId: user._id })
+        res.status(200).send(token)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+
+
 })
 
 const PORT = 5000
